c004145acda7f5072c1bc8f5604795acdfa20c62
Mauro Junior <mcaj20@gmail.com>
2023-05-25 21:23:23 -0300

    Praticando

---
+++ index.js
@@ -0,0 +1,4 @@
+function anime() {
+  document.getElementById("M").innerHTML = "Há sempre a escolha entre voltar atrás para a segurança ou seguir em frente para o crescimento. O crescimento deve ser escolhido uma, duas, três e infinitas vezes."
+
+}
